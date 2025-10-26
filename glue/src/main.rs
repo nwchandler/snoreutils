a79e3e0fff287f1132e75737fc591ef0966dbecd
Nick Chandler <nwchandler@users.noreply.github.com>
2025-10-25 22:19:29 -0400

    Initial glue Implementation

---
+++ glue/src/main.rs
@@ -0,0 +1,140 @@
+use std::{fs::File, io::ErrorKind};
+
+use anyhow::Result;
+use binrw::{BinRead, BinWrite};
+use clap::{Parser, Subcommand};
+use prettytable::{Table, format, row};
+
+use glue::Archive;
+
+const DEFAULT_ARCHIVE_FILE: &str = "archive.glue";
+const DEFAULT_PREVIEW_SIZE: usize = 64;
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Create {
+            archive: target,
+            source,
+        } => {
+            let archive = Archive::from_paths(source)?;
+            let mut output = File::create(target)?;
+            archive.write(&mut output)?;
+        }
+
+        Commands::Inspect {
+            archive: target,
+            preview_size,
+        } => {
+            let preview_size = *preview_size;
+            let mut reader = File::open(target)?;
+            let archive = Archive::read(&mut reader)?;
+
+            println!("{}:", target);
+            let mut table = Table::new();
+            table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+            let titles = row![
+               "NAME",
+                c->"SIZE",
+               "PREVIEW",
+            ];
+            table.set_titles(titles);
+
+            for record in archive.records {
+                let preview = match record.preview(preview_size as u32) {
+                    Some(preview) => match preview {
+                        glue::Preview::String {
+                            mut preview,
+                            truncated,
+                        } => {
+                            if truncated {
+                                preview += "...";
+                            }
+                            preview
+                        }
+                        glue::Preview::Data => String::from("<data>"),
+                    },
+                    None => String::from("<empty>"),
+                };
+                table.add_row(row![
+                    record.filename,
+                    r->format!("{} bytes", record.size),
+                    preview
+                ]);
+            }
+
+            table.printstd();
+        }
+
+        Commands::Extract { archive, keep } => {
+            let mut reader = File::open(archive)?;
+            let archive = Archive::read(&mut reader)?;
+
+            for record in archive.records {
+                let filename = &record.filename;
+                let mut file = if *keep {
+                    match File::create_new(filename.to_string()) {
+                        Ok(f) => f,
+                        Err(e) => match e.kind() {
+                            ErrorKind::AlreadyExists => continue,
+                            _ => {
+                                return Err(e.into());
+                            }
+                        },
+                    }
+                } else {
+                    File::create(filename.to_string())?
+                };
+                record.extract(&mut file)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+/// A lightweight file archive / unarchive utility.
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a glue archive out of source files.
+    ///
+    /// The path will be elided in the output archive, keeping
+    /// only the base name of the provided source(s).
+    Create {
+        #[arg(short, long, default_value = DEFAULT_ARCHIVE_FILE)]
+        /// The path to the output archive file.
+        archive: String,
+
+        /// The input files to add to the archive.
+        #[arg(required = true)]
+        source: Vec<String>,
+    },
+
+    /// Inspect the contents of an archive file.
+    Inspect {
+        /// The archive file you want to inspect.
+        #[arg(required = true)]
+        archive: String,
+
+        /// The length of text to preview in the output table.
+        #[arg(short, long, default_value_t = DEFAULT_PREVIEW_SIZE)]
+        preview_size: usize,
+    },
+
+    /// Extract the contents of an archive file.
+    Extract {
+        /// The archive file you want to extract.
+        #[arg(required = true)]
+        archive: String,
+
+        /// Do not overwrite existing output file contents.
+        #[arg(short, long, default_value_t = false)]
+        keep: bool,
+    },
+}
